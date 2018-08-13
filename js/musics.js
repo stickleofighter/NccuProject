4d70cd999c1e09f50e7976a20caeca5a800959a1
ZX02-Hsiao <stickleofighter@gmail.com>
2018-08-13 12:51:29 +0800

    voice

--- js/musics.js
+++ js/musics.js
@@ -1,1 +1,21 @@
-function voiceControl()
+var bgm;
+var vo1;
+var vo2;
+function voiceControl(kind)
+{
+	let voice=$(kind);
+	voice[0].volume=0.5;
+	return{
+		muteswitch:()=>{
+			voice[0].muted=!voice[0].muted;
+		},
+		volumeset:(num)=>{
+			voice[0].volume=num*0.005;
+		}
+	}
+}
+$(document).ready(()=>{
+	bgm=voiceControl("#bgMusic");
+	vo1=voiceControl("#bgSound1");
+	vo2=voiceControl("#bgSound2");
+})
