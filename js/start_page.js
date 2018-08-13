d537a539124a0c9ee3e8dfc01114704040174f22
ZX02-Hsiao <stickleofighter@gmail.com>
2018-08-13 21:01:30 +0800

    bugtest

--- js/start_page.js
+++ js/start_page.js
@@ -79,9 +79,8 @@ function touchEventHandler()
 	let MouseClickHandler=e=>
 	{
 		Mpos=getMousePos(e);
-		/*for(let i in buttons)
-			if(areaCheck(buttons[i].x,buttons[i].y,buttons[i].w,buttons[i].h))alert("ouch!");*/
-		alert("ouch!");
+		for(let i in buttons)
+			if(areaCheck(buttons[i].x,buttons[i].y,buttons[i].w,buttons[i].h))alert("ouch!");
 	};
 	/*ctouchcheck.on("mousemove",MouseMoveHandler);
 	ctouchcheck.on("mousedown",MouseDownHandler);
