a867d9a531d3ad5d295ade6009fa194e5112e764
ZX02-Hsiao <stickleofighter@gmail.com>
2018-09-02 03:08:03 +0800

    team_page_1

--- js/team_page.js
+++ js/team_page.js
@@ -8,50 +8,6 @@ var cbtn;
 var bg;
 var check_btn;
 
-function BG()
-{
-	let Images=new Image();
-	Images.src="../pic/main_page/team_page.png";
-	ImageArray.push(Images);
-	let xx=0;
-	let yy=0;
-	let ww=750;
-	let hh=420;
-	return{
-		draw: ()=>{cbg.drawImage(Images,xx,yy,ww,hh);}
-	};
-}
-function CHECK(url,x,y,w,h)
-{
-	let Images=new Image();
-	let Images2=new Image();
-	Images.src=`media/pic/main_page/${url}_1.png`;
-	Images2.src=`media/pic/main_page/${url}_2.png`;
-	ImageArray.push(Images);
-	ImageArray.push(Images2);
-	let PlayOnce=true;
-	let ww=[w,w*0.9,w*1.1];
-	let hh=[h,h*0.9,h*1.1];
-	let xx=[x,x+(ww[0]-ww[1])/2,x-(ww[2]-ww[0])/2];
-	let yy=[y,y+(hh[0]-hh[1])/2,y-(hh[2]-hh[0])/2];
-	return{
-		get x(){return xx;},
-		get y(){return yy;},
-		get w(){return ww;},
-		get h(){return hh;},
-		get MovePlayOnce(){return PlayOnce;},
-		set MovePlayOnce(tf){PlayOnce=tf;},
-		drawNotCheck: (bs)=>{
-			cbtn.clearRect(xx[2],yy[2],ww[2],hh[2]);
-			cbtn.drawImage(Images,xx[bs],yy[bs],ww[bs],hh[bs]);
-		},
-		drawIsCheck: (bs)=>{
-			cbtn.clearRect(xx[2],yy[2],ww[2],hh[2]);
-			cbtn.drawImage(Images2,xx[bs],yy[bs],ww[bs],hh[bs]);
-		}
-	};
-}
-
 function touchEventHandler()
 {
 	let Mpos;
