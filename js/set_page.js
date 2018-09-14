df0769b9a246fef0a2bd2ae6f969f8a7fe4c09c9
stickleofighter <stickleofighter@gmail.com>
2018-09-14 12:36:23 +0800

    bug_fix

--- js/set_page.js
+++ js/set_page.js
@@ -39,7 +39,7 @@ function touchEventHandler()
 		if(volControl.isDown())
 		{
 			if(volControl.x!=Mpos.x)volControl.sx(Mpos.x-volControl.w/2);
-			else volControl.sx(parseInt(volControl.x)+parseInt(Mpos.x-Mpos.prex);)
+			else volControl.sx(parseInt(volControl.x)+parseInt(Mpos.x-Mpos.prex));
 			volControl.sx(volControl.x>volControl.limit.right?volControl.limit.right:volControl.x);
 			volControl.sx(volControl.x<volControl.limit.left?volControl.limit.left:volControl.x);
 			drawMusic();
